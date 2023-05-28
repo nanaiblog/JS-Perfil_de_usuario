f386f38b0f8acff1f345077a5be7b76fc6c37277
Nanai <130193531+nanaiblog@users.noreply.github.com>
2023-05-27 23:52:04 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -14,9 +14,31 @@ function cambioNombre(linkElement) {
 }
 
 function hideUser(buttonElement) {
-    var userDiv = buttonElement.closest(".user");
+    var userDiv = buttonElement.parentElement.parentElement;
     if (userDiv) {
       userDiv.style.display = "none";
     }
   }
-  
+
+//console.log("page loaded...");
+
+//var requestSpan = document.querySelector("#requests");
+//var connectionSpan = document.querySelector("#connections");
+//var username = document.querySelector("#username");
+
+//function accept(id) {
+ //   var element = document.querySelector(id);
+//    element.remove();
+//    requestSpan.innerText--;
+//    connectionSpan.innerText++;
+//}
+
+//function ignore(id) {
+//    var element = document.querySelector(id);
+//    element.remove();
+//    requestSpan.innerText--;
+//}
+
+//function edit() {
+//    username.innerText = "Marisa G";
+//}
